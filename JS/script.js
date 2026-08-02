d72685e946cda1af4e80edc47c19037c5c455489
Saif Mohamed <sofamekawi@gmail.com>
2026-08-02 21:40:23 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+console.log("JS is running successfully");
+window.alert("Ready to practice");
+
+var productName = "Laptop";
+var price = 1500;
+var isAailable = true;
+console.log(productName);
+console.log(typeof productName);
+console.log(price);
+console.log(typeof price);
+console.log(isAailable);
+console.log(typeof isAailable);
